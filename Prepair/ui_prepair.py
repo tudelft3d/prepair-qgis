948a04419209cdb260d5c1863d8b6d9d443564e6
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-03-05 16:53:03 +0100

    logo updated

--- Prepair/ui_prepair.py
+++ Prepair/ui_prepair.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_prepair.ui'
 #
-# Created: Mon Mar  3 16:55:30 2014
+# Created: Wed Mar  5 16:46:29 2014
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
